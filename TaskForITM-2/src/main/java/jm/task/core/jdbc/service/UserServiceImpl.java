7a79563dc1f6e0ede6a897180d9a22a3bd0c2fa3
RodUman1985 <Vand_85@mail.ru>
2025-05-03 20:04:57 +0300

    7

--- TaskForITM-2/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ TaskForITM-2/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,7 +8,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-
+//параметр true - hibernate параметр false - jdbc
     UserDao userDao = getUserDao(true);
 
     private UserDao getUserDao(boolean isHibernate) {
